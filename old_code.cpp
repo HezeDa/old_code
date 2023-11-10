d5330f34c8fd320f4809704c018dfe548c2a0468
Nikita_Bulatov <tricion219@gmail.com>
2023-11-10 17:42:16 +0300

    Add code in the main function

--- old_code.cpp
+++ old_code.cpp
@@ -5,11 +5,44 @@
 #include <Windows.h>
 #include <string.h>
 
-void search_word_in_string();
+void search_word_in_string(const char* string, int* counter, const char* words[]);
 
 int main()
 {
 
+	// Люблю писать на русском языке
+	setlocale(LC_ALL, "Rus"); // Это для вывода слов
+	SetConsoleCP(1251);          // С этой хренью начинает работать
+	SetConsoleOutputCP(1251);    // русскоязычный ввод (надо будет погуглить)
+
+	// Объявление переменных
+	char string[81] = ""; // строка -_-
+	const char* words[40]; // массив слов из строки
+	int counter = 0; // счетчик слов в строке
+
+	// Получить строку из консоли
+	fgets(string, 81, stdin);
+
+	// Вызов функции
+	search_word_in_string(string, &counter, words);
+
+	// Вывод слов
+	for (int i = 0; i < counter; i++)
+	{
+		// Вывести слово
+		while (*words[i] != '.' and *words[i] != ',' and *words[i] != ' ' and *words[i] != '\0')
+		{
+			printf("%c", *words[i]); // Вывести символ из слова
+			words[i]++; // Перейти к следующему символу в слове
+		}
+
+		printf("\n"); // перейти к адресу следующего слова
+	}
+
+	if (counter == 0)
+		printf("строка пустая -_-\n"); // вывод в случае того, если слов в строке нет
+
+	return 0;
 
 }
 
